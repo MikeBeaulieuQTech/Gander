704a5d0b45d045177b84ffce49d98f645bbb05fa
Selvanathan, Sam <sjasel@gmail.com>
2015-07-29 21:55:10 -0700

    added cron job to sync

--- service/sync.js
+++ service/sync.js
@@ -1,7 +1,33 @@
 'use strict';
 
 import logger from './log'; // instantiate the logger
-import {sync} from './index';
+import {sync, fetchAll} from './index';
+import {CronJob} from 'cron';
 
-logger.debug('starting sync');
-sync();
+const CRON_TIME = '00 01 * * * *'; // run every hour
+
+function onTick() {
+  logger.info('cron:onTick');
+  sync();
+}
+
+function onComplete() {
+  logger.info('cron:onComplete');
+}
+
+logger.info('cron:instantiate');
+new CronJob(CRON_TIME, onTick, onComplete, true, 'America/Los_Angeles');
+
+// if the db is empty run the sync immediately
+fetchAll(function(err, results) {
+
+  if (err) {
+    // using err to see if the records are there in db
+    // figure out a better way to implement this
+    // {"message":"Key not found in database [paypal]","name":"NotFoundError","stack":"NotFoundError: Key not found in database [paypal]\n    at
+    logger.info(err);
+    // start the sync to replenish db
+    sync();
+    return;
+  }
+});
