f2592e99a7220ed7d4da724da038dcfc4f595c85
Selvanathan, Sam <sjasel@gmail.com>
2015-07-30 15:15:00 -0700

    make cron sync start as part of server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
 'use strict';
 
-var app = require('./index');
 var http = require('http');
+var app = require('./index');
 
+require('./service/sync');
 require('node-jsx').install({extension: '.jsx'});
-
-var server;
 
 /*
  * Create and start HTTP server.
  */
-
-server = http.createServer(app);
+var server = http.createServer(app);
 server.listen(process.env.PORT || 8000);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
